230f29474efe663929e2063851b980e0419d3766
Chris <chrislo.7@live.com>
2018-09-23 21:47:39 +0000

    added axios call to server to grab db data

---
+++ src/components/blogs/article-list.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ArticleEntry from "./article-entry.jsx"
+
+const ArticleList = (props) => {
+  return (
+    <div>
+      <ArticleEntry />
+    </div>
+  )
+}
+
+export default ArticleList;
