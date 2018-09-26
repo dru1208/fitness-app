7f3e1c7220d2dac364151959b387ba7c64aba536
Chris <chrislo.7@live.com>
2018-09-26 00:57:43 +0000

    merging

--- src/components/fitness-events/event-entry.jsx
+++ src/components/fitness-events/event-entry.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import axios from 'axios'
 
-
-
-
 export default class EventEntry extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: []
+      data: [],
+      id: 1
     }
   }
 
@@ -20,15 +18,28 @@ export default class EventEntry extends React.Component {
       })
   }
 
+  _handleDestroy = (e) => {
+    e.preventDefault();
+    let name = this.state.name
+    let description = this.state.description
+    let location = this.state.location
+    let datetime = this.state.datetime
+    let id = this.state.id
+    axios.delete('http://localhost:3000/api/events/:id', { id: id })
+  }
+
   render() {
     return (
       <div>
       { this.state.data.map(entries =>
-        <div className="single-event">
-          <a href="https://www.w3schools.com">{entries.name}</a>
-          <div className="event-description">{entries.description}</div>
-          <div className="event-datetime-location">{entries.location} • {entries.datetime}</div>
-        </div>
+        <form onSubmit={this._handleDestroy}>
+          <div className="single-event">
+            <span className="event-name">{entries.name}</span>
+            <div className="event-description">{entries.description}</div>
+            <div className="event-datetime-location">{entries.location} • {entries.datetime}</div>
+            <input type="submit" value="Delete Event"/>
+          </div>
+        </form>
       )}
       </div>
     )
