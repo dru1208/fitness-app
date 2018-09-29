a5070cd5a157799147e7697f0405a512c9a6546e
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-09-29 06:10:08 +0000

    work on nutrition natural language input

--- src/components/nutrition/nutrition-main.jsx
+++ src/components/nutrition/nutrition-main.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios'
 import NutritionInput from "./nutrition-input.jsx"
 import ImageUpload from "./nutrition-upload-image.jsx"
 
@@ -23,6 +24,47 @@ export default class Dashboard extends Component {
     };
   }
 
+
+  _submitNutritionHandler = (e) => {
+
+    e.preventDefault();
+    const formInput = e.target.elements
+    const inputDate = formInput.date.value.split('-')
+    let datetime;
+    const findDatetime = (date, hour) => {
+      return new Date(Number(date[0]), Number(date[1]), Number(date[2]), hour)
+    }
+    switch (formInput.mealTime.value) {
+      case "breakfast":
+        datetime = findDatetime(inputDate, 7);
+        break;
+      case "lunch":
+        datetime = findDatetime(inputDate, 12);
+        break;
+      case "dinner":
+        datetime = findDatetime(inputDate, 18);
+        break;
+    }
+    console.log(datetime)
+    axios.post('http://localhost:3000/api/user_nutritions', {
+      user_id: this.props.userID,
+      calories: formInput.calories.value ? formInput.calories.value : 0,
+      protein: formInput.protein.value ? formInput.protein.value : 0,
+      fat: formInput.fat.value ? formInput.fat.value : 0,
+      carbohydrates: formInput.carbohydrates.value ? formInput.carbohydrates.value : 0,
+      sodium: formInput.sodium.value ? formInput.sodium.value : 0,
+      cholesterol: formInput.cholesterol.value ? formInput.cholesterol.value : 0,
+      serving_size: formInput.servingSize.value ? formInput.servingSize.value : 1,
+      datetime: datetime,
+      meal_query: formInput.mealType.value ? formInput.mealType.value : ""
+    })
+    .then((response) => {
+      this.setState({nutrition: response.data})
+    })
+  }
+
+
+
   render() {
     return (
       <main className="nutrition-page">
@@ -30,7 +72,7 @@ export default class Dashboard extends Component {
         <div className="watson">
         This is where watson api will go.
         </div>
-        <NutritionInput userID={this.state.userID}/>
+        <NutritionInput userID={this.state.userID} submitNutritionHandle={this._submitNutritionHandler}/>
         <ImageUpload/>
       </main>
     );
