d4e526019aa8d01b1f37818c13b7d56444be4e3c
Chris <chrislo.7@live.com>
2018-10-01 20:25:53 +0000

    added in slideout nav bar with animations, fixed pageLayout to accomodate for new navbar

--- src/App.jsx
+++ src/App.jsx
@@ -112,9 +112,11 @@ class App extends Component {
 
           <Route exact path={generateUserURL(this.state.userID, "dashboard")}
             render={() => (this.state.currentUser ?
-              (<div className="pageLayout">
-                <NavBar handleLogout={this._handleLogout} id={this.state.userID}/>
-                <Dashboard name={this.state.currentUser} userID={this.state.userID}/>
+              (<div id="App" className="pageLayout">
+                <NavBar pageWrapId={"page-wrap"} outerContainerId={"App"} handleLogout={this._handleLogout} id={this.state.userID} />
+                <div id="page-wrap">
+                  <Dashboard name={this.state.currentUser} userID={this.state.userID}/>
+                </div>
               </div>) :
               <Redirect to="/" />
             )}
@@ -122,9 +124,11 @@ class App extends Component {
 
           <Route exact path={generateUserURL(this.state.userID, "map")}
             render={() => (this.state.currentUser ?
-              (<div className="pageLayout">
-                <NavBar handleLogout={this._handleLogout} id={this.state.userID}/>
-                <Maps jwt={this.state.jwt}/>
+              (<div id="App" className="pageLayout">
+                <NavBar pageWrapId={"page-wrap"} outerContainerId={"App"} handleLogout={this._handleLogout} id={this.state.userID}/>
+                <div id="page-wrap">
+                  <Maps jwt={this.state.jwt}/>
+                </div>
               </div>) :
               <Redirect to="/" />
             )}
@@ -132,9 +136,11 @@ class App extends Component {
 
           <Route exact path={generateUserURL(this.state.userID, "nutrition")}
             render={() => (this.state.currentUser ?
-              (<div className="pageLayout">
-                <NavBar handleLogout={this._handleLogout} id={this.state.userID}/>
-                <Nutrition userID={this.state.userID} name={this.state.currentUser} id={this.state.userID} jwt={this.state.jwt}/>
+              (<div id="App" className="pageLayout">
+                <NavBar pageWrapId={"page-wrap"} outerContainerId={"App"} handleLogout={this._handleLogout} id={this.state.userID}/>
+                <div id="page-wrap">
+                  <Nutrition userID={this.state.userID} name={this.state.currentUser} id={this.state.userID} jwt={this.state.jwt}/>
+                </div>
               </div>) :
               <Redirect to="/" />
             )}
@@ -142,9 +148,11 @@ class App extends Component {
 
           <Route exact path={generateUserURL(this.state.userID, "blog")}
             render={() => (this.state.currentUser ?
-              (<div className="pageLayout">
-                <NavBar handleLogout={this._handleLogout} id={this.state.userID}/>
-                <BlogMain name={this.state.currentUser} userID={this.state.userID}/>
+              (<div id="App" className="pageLayout">
+                <NavBar pageWrapId={"page-wrap"} outerContainerId={"App"} handleLogout={this._handleLogout} id={this.state.userID}/>
+                <div id="page-wrap">
+                  <BlogMain name={this.state.currentUser} userID={this.state.userID}/>
+                </div>
               </div>) :
               <Redirect to="/" />
             )}
@@ -153,9 +161,11 @@ class App extends Component {
 
           <Route exact path={generateUserURL(this.state.userID, "events")}
             render={() => (this.state.currentUser ?
-              (<div className="pageLayout">
-                <NavBar handleLogout={this._handleLogout} id={this.state.userID}/>
-                <EventMain userID={this.state.userID} />
+              (<div id="App" className="pageLayout">
+                <NavBar pageWrapId={"page-wrap"} outerContainerId={"App"} handleLogout={this._handleLogout} id={this.state.userID}/>
+                <div id="page-wrap">
+                  <EventMain userID={this.state.userID} />
+                </div>
               </div>) :
               <Redirect to="/" />
             )}
@@ -163,9 +173,11 @@ class App extends Component {
 
           <Route exact path={generateUserURL(this.state.userID, "recent")}
             render={() => (this.state.currentUser ?
-              (<div className="pageLayout">
-                <NavBar handleLogout={this._handleLogout} id={this.state.userID}/>
-                <Recent />
+              (<div id="App" className="pageLayout">
+                <NavBar pageWrapId={"page-wrap"} outerContainerId={"App"} handleLogout={this._handleLogout} id={this.state.userID}/>
+                <div id="page-wrap">
+                  <Recent />
+                </div>
               </div>) :
               <Redirect to="/" />
             )}
