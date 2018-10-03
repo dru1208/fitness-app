0e6b9330a2c7a2449ac91ca912b0ca492d9c05f2
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-03 09:43:00 +0000

    article link now opens to a new tab and goes to the right url instead of localhost/url

--- src/components/blogs/article-entry.jsx
+++ src/components/blogs/article-entry.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import axios from 'axios'
 
 const ArticleEntry = (props) => {
+  console.log(props.article.link)
   return (
     <div className="articleEntryWrapper">
       <article className="articleEntry border">
         <h3 className="articleEntryHeader">{props.article.title}</h3>
-        <a className="articleEntryLink" href={props.article.link}>{props.article.link}</a>
+        <a className="articleEntryLink" href={"//" + props.article.link} target="_blank">{props.article.link}</a>
         <form onSubmit={props.handleArticleDestroy}>
           <input type="hidden" name="articleID" value={props.article.id} />
           <input className="articleEntryDelete" type="submit" value="Delete" />
