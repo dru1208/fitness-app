eb11e127ac2913d22e77790380cffeb9a83f91b1
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-03 13:37:51 +0000

    nutrition chart bug fixed

--- src/components/dashboard/nutrition.jsx
+++ src/components/dashboard/nutrition.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Chart } from "react-google-charts";
 import axios from "axios";
-import NutritionChart from './nutrition-chart.jsx'
 
 export default class Nutrition extends Component {
   constructor(props) {
@@ -21,7 +20,7 @@ export default class Nutrition extends Component {
     .then(response => {
       const data = response.data;
       console.log(data)
-      if (response.data.length > 0) {
+      if (data) {
         this.setState({
           nutrition: {
             calories: data.calories,
@@ -38,29 +37,38 @@ export default class Nutrition extends Component {
   }
 
   render() {
-    const generateNutritionChart = () => {
-      if (this.state.nutrition) {
-        return (
-          <NutritionChart
-            protein={this.state.nutrition.protein}
-            fat={this.state.nutrition.fat}
-            carbohydrates={this.state.nutrition.carbohydrates}
-            pieOptions={pieOptions}
-            calories={this.state.nutrition.calories}
+    const generateNutritionChart = (options) => {
+      return (
+        <div className="dashboardCharts">
+          <Chart
+            chartType="PieChart"
+            data={[
+              ["Nutrition", "Calories"],
+              ["Protein", this.state.nutrition.protein * 4],
+              ["Fat", this.state.nutrition.fat * 9],
+              ["Carbohydrates", this.state.nutrition.carbohydrates * 4]
+              // ["Cholesterol", this.state.nutrition.cholesterol],
+              // ["Sugar", this.state.nutrition.sugar],
+              // ["Sodium", this.state.nutrition.sodium]
+            ]}
+            options={pieOptions}
+            graph_id="PieChart"
+            width={"100%"}
+            height={"400px"}
+            legend_toggle
           />
-        )
-      }
+          <h2>Total calories: {this.state.nutrition.calories}</h2>
+        </div>
+      )
     }
 
     const generateNoNutritionMessage = () => {
-      if (!this.state.nutrition) {
-        return(
-          <div className="dashboardCharts">
-            <h3>Hey There!</h3>
-            <h4>From our data, it looks like you haven't eaten in a week - go get some food you must be famished. It's time to get off your butt and log your food!</h4>
-          </div>
-        )
-      }
+      return(
+        <div className="dashboardCharts">
+          <h3>Hey There!</h3>
+          <h4>From our data, it looks like you haven't eaten in a week - go get some food you must be famished. It's time to get off your butt and log your food!</h4>
+        </div>
+      )
     }
 
 
@@ -118,8 +126,16 @@ export default class Nutrition extends Component {
 
     return (
       <main className="dashboardNutrition border">
-        {generateNutritionChart()}
-        {generateNoNutritionMessage()}
+        {this.state.nutrition ?
+          <div className="dashboardCharts">
+            {generateNutritionChart(pieOptions)}
+          </div>
+        :
+          <div className="dashboardCharts">
+            <h3>Hey There!</h3>
+            <h4>From our data, it looks like you haven't eaten in a week - go get some food you must be famished. It's time to get off your butt and log your food!</h4>
+          </div>
+        }
       </main>
     )
   }
